a31f139ec2c0fb3760caadf479aa76f7cdec487c
Beny <dannymatthew@gmail.com>
2021-03-29 13:22:42 +0530

    Update stack.spec.js

--- src/__tests__/stack.spec.js
+++ src/__tests__/stack.spec.js
@@ -3,12 +3,20 @@ import { Stack } from '../index.js';
 describe('Testing the Stack Data Type', () => {
 	const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 	const length = numbers.length;
+	let stack;
+
+	beforeEach(() => {
+		stack = new Stack();
+	});
 
 	test('It should be able to push items', () => {
-		const stack = new Stack();
-
 		numbers.forEach((number) => stack.push(number));
+		const stackToArray = stack
+			.toString()
+			.split(',')
+			.map((strInt) => parseInt(strInt));
 
 		expect(stack.size()).toBe(length);
+		expect(stackToArray).toEqual(numbers);
 	});
 });

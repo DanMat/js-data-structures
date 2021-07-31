7d62cee1c1e39c5d9ebd3d1c37fa5a620257b3bf
DanMat <dannymatthew@gmail.com>
2021-07-31 09:53:25 -0400

    Update linked-list.js
    - Implemented a removeAtIndex method

--- src/linked-list.js
+++ src/linked-list.js
@@ -11,20 +11,54 @@ class LinkedList {
 		this.count = 0;
 	}
 
+	isIndexOutOfBound(index) {
+		// Make sure that the index is less
+		// than the node count we maintain.
+		return !(index >= 0 && index < this.count);
+	}
+
+	nodeAtIndex(index) {
+		if (this.isIndexOutOfBound(index)) return null;
+		let node = this.head;
+		// Loop to index to get the node
+		for (let i = 0; i < index; i += 1) {
+			node = node.next;
+		}
+		return node;
+	}
+
 	push(element) {
 		const node = new Node(element);
 		if (this.head === null) {
 			// This handles the first push
 			this.head = node;
 		} else {
-			let current = this.head;
+			let currentNode = this.head;
 			// Iterate to the last item
-			while (current.next !== null) {
-				current = current.next;
+			while (currentNode.next !== null) {
+				currentNode = currentNode.next;
 			}
 			// Assign the new node to the end
-			current.next = node;
+			currentNode.next = node;
 		}
+		this.count += 1;
+	}
+
+	removeAtIndex(index) {
+		if (this.isIndexOutOfBound(index)) return null;
+		let currentNode = this.head;
+		if (index === 0) {
+			// Handle first node
+			this.head = currentNode.next;
+		} else {
+			const previousNode = this.nodeAtIndex(index - 1);
+			currentNode = previousNode.next;
+			// We detach the node at index by re-pointing the previous
+			// node to the one the node at index is pointing to
+			previousNode.next = currentNode.next;
+		}
+		this.count -= 1;
+		return currentNode.element;
 	}
 }
 

93152d7e2af9be97abf3ecd94230dd48572b2bb0
DanMat <dannymatthew@gmail.com>
2021-07-31 15:47:10 -0400

    Adding tests for linkedlist removal

--- src/__tests__/linked-list.spec.js
+++ src/__tests__/linked-list.spec.js
@@ -5,9 +5,14 @@ describe('Testing the LinkedList Data Type', () => {
 	const { length } = numbers;
 	let linkedList;
 
+	const setupLinkedList = () => {
+		const linkedListInstance = new LinkedList();
+		numbers.forEach((number) => linkedListInstance.push(number));
+		return linkedListInstance;
+	};
+
 	beforeEach(() => {
-		linkedList = new LinkedList();
-		numbers.forEach((number) => linkedList.push(number));
+		linkedList = setupLinkedList();
 	});
 
 	test('It should be able to push items', () => {
@@ -20,14 +25,45 @@ describe('Testing the LinkedList Data Type', () => {
 		expect(linkedListToArray).toEqual(numbers);
 	});
 
-	test('It should be able to insert by index', () => {
+	// test('It should be able to insert by index', () => {
+	// 	// Reset the linkedList instance
+	// 	linkedList = new LinkedList();
+	// 	for (let index = 0; index < numbers.length; index++) {
+	// 		const element = numbers[index];
+	// 		linkedList.insertAtIndex(index, element);
+	// 		// Make sure the index matches
+	// 		expect(linkedList.indexOf(element)).toBe(index);
+	// 		// Make sure that the element at index matches
+	// 		expect(linkedList.nodeAtIndex(index).element).toBe(element);
+	// 		// Make sure the size matches
+	// 		expect(linkedList.size()).toBe(index + 1);
+	// 	}
+	// });
+
+	test('It should be able to delete by index', () => {
 		for (let index = 0; index < numbers.length; index++) {
-			const element = numbers[index];
-			linkedList.insertAtIndex(index, element);
-			// Make sure the index matches
-			expect(linkedList.indexOf(element)).toBe(index);
-			// Make sure that the element at index matches
-			expect(linkedList.nodeAtIndex(index).element).toBe(element);
+			// Reset linkedList for each test
+			linkedList = setupLinkedList();
+			const removedElement = linkedList.removeAtIndex(index);
+
+			expect(removedElement).toBe(numbers[index]);
 		}
+
+		// Remove out of bound index
+		expect(linkedList.removeAtIndex(100)).toBe(null);
+	});
+
+	test('It should be able to delete by element', () => {
+		for (let index = 0; index < numbers.length; index++) {
+			// Reset linkedList for each test
+			linkedList = setupLinkedList();
+			const element = numbers[index];
+			const removedElement = linkedList.remove(element);
+
+			expect(removedElement).toBe(element);
+		}
+
+		// Remove out of bound index
+		expect(linkedList.removeAtIndex(100)).toBe(null);
 	});
 });
